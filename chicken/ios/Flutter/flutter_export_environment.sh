0e10d9bdc3a7f4e088a533e5325720aab2e36ef7
rishabnayak <rishab@bu.edu>
2020-06-11 20:01:20 -0400

    logo

--- chicken/ios/Flutter/flutter_export_environment.sh
+++ chicken/ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/rishabnayak/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/rishabnayak/Desktop/Projects/boston-hacks-2019-day-of/chicken"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/rishabnayak/Desktop/Projects/boston-hacks-2019-day-of/chicken/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/rishabnayak/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/Users/rishabnayak/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=3"
+export "FLUTTER_BUILD_NUMBER=2019"
+export "TRACK_WIDGET_CREATION=true"
